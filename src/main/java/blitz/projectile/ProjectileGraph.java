7882de599374b1a27e795a96190d50fa71507b6c
chanablitz <cblitz2@student.touro.edu>
2024-02-28 16:17:43 -0500

    fixed oval

--- src/main/java/blitz/projectile/ProjectileGraph.java
+++ src/main/java/blitz/projectile/ProjectileGraph.java
@@ -22,7 +22,7 @@ public class ProjectileGraph extends JComponent {
         }
 
         g.setColor(Color.BLUE);
-        g.fillOval((int) projectile.getInterceptX() / 2, (int) -projectile.getPeakY(), 10, 10);
+        g.fillOval((int) projectile.getInterceptX() / 2 - 5, (int) -projectile.getPeakY() - 5, 10, 10);
     }
 
     public void setProjectile(Projectile projectile) {
